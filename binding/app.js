1c75f462eab0770346ec31347b820e5764c826c9
Kevin Warnock <kevi.warnock@gmail.com>
2015-11-01 14:13:15 -0500

    Initial commit for the repository. Also added project demonstrating one/two way data binding in AngularJS 1.3

---
+++ binding/app.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+
+    angular.module('binding.app', ['ngAnimate'])
+        .controller('MainController', ['$timeout', createMainController]);
+
+    //Define the MainController function
+    function createMainController($timeout) {
+        //Store the context in a variable that matches the reference on the view to make it easier to follow
+        var main = this;
+
+        _slickReveal();
+
+        //Create a default value for the description property (Prevents confusion)
+        main.description = '';
+        main.descriptionLabel = 'Description';
+        main.headerText = 'Binding with AngularJS';
+
+        //Define the functions extended to the view
+        //NOTE: When the controller gets larger this makes it much easier to follow the code logic.
+        main.descriptionCheck = descriptionCheck;
+
+        //Define functions
+        function descriptionCheck() {
+            return angular.isDefined(main.description) && !_isEmpty(main.description);
+        }
+
+        //Functions that are not exposed should be denoted with an '_' in front of their name
+        function _slickReveal() {
+            $timeout(function() {
+                angular.element(document.body).addClass('active');
+            }, 50);
+
+            $timeout(function() {
+                angular.element(document.querySelector('header')).addClass('active');
+            }, 300);
+        }
+
+        function _isEmpty(str) {
+            return str.length === 0;
+        }
+    }
+})();
